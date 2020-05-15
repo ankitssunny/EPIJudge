406661a7d727dc26d6801f4f5382aec94b2db8b9
Ankit Sharma <ankitssunny@gmail.com>
2020-05-15 10:32:24 -0400

    Solved furthestReachable
    Did it with help from book. Took 2 days to think through but still was not able to. This is not a harsh statement but to ground yourself in reality that things take time so start early.

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -2,12 +2,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
     // TODO - you fill in here.
-    return true;
+    int furthestReachable = 0, thisReachable = 0;
+
+    for (int i = 0 ; i <= furthestReachable; i++) {
+      if (furthestReachable < maxAdvanceSteps.size() - 1 ){
+        thisReachable = i + maxAdvanceSteps.get(i);
+        if (thisReachable > furthestReachable) furthestReachable = thisReachable;
+      }
+    }
+    return furthestReachable >= maxAdvanceSteps.size() - 1;
   }
 
   public static void main(String[] args) {
